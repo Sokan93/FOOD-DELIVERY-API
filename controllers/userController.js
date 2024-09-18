a8cd0cca62e81d9389c500e72fb9880801e53c49
Sokan93 <ayodejisokan@gmail.com>
2024-09-18 04:09:22 -0700

    Updated routes and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,59 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+
+
+const registerUser = async (req, res) => {
+  const {username, email, password} = req.body
+
+  if(!username || !email || !password) {
+    res.status(400).json({message: "Please enter your credentials"})
+  }
+
+  //Check if user exists
+  const userExists = await User.findOne({email})
+
+  if(userExists) {
+    res.status(400).json({message: "User already exists"})
+  }
+
+  const salt = await bcrypt.genSalt(12)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password //testing to confirm password hashing 
+    })
+  } else {
+    res.status(400).json({message: 'Invalid user data'})
+  }
+}
+
+const loginUser = async(req, res) => {
+  const {email, password} = req.body
+  const user = await User.findOne({email})
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+    })
+  } else {
+    res.status(400).json({message: "Invalid Login Details"})
+  }
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+}
--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const http = require("http");
 const express = require('express');
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose")
-const User = require("./models/userModel")
 const Restaurant = require("./models/restaurantModel")
 const Menu = require("./models/menuModel")
 const Order = require("./models/orderModel")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
+
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); //Takes out query parameters from URL on Postman
+app.use('/api', require('./routes/userRoutes'))
+
 
 const PORT = process.env.PORT
 
@@ -25,63 +25,6 @@ app.listen(PORT, () => {
 mongoose.connect(`${process.env.MONGODB_URL}`)
   .then(() => console.log('Database Connected!'));
 
-//User Registration
-app.post('/api/register-user', async (req, res) => {
-  const {username, email, password} = req.body
-
-  if(!username || !email || !password) {
-    res.status(400).json({message: "Please enter your credentials"})
-  }
-
-  //Check if user exists
-  const userExists = await User.findOne({email})
-
-  if(userExists) {
-    res.status(400).json({message: "User already exists"})
-  }
-
-  const salt = await bcrypt.genSalt(12)
-  const hashedPassword = await bcrypt.hash(password, salt)
-
-  //Create User
-  const user = await User.create({
-    username,
-    email,
-    password: hashedPassword
-  })
-
-  if(user) {
-    res.status(201).json({
-      _id: user.id,
-      username: user.username,
-      email: user.email,
-      password: user.password //testing to confirm password hashing 
-    })
-  } else {
-    res.status(400).json({message: 'Invalid user data'})
-  }
 
 
-
-})
-
-//Login User
-app.post('/api/login-user', async (req, res) => {
-  const {email, password} = req.body
-  const user = await User.findOne({email})
-  if(user && (await bcrypt.compare(password, user.password))) {
-    res.status(200).json({
-      _id: user.id,
-      username: user.username,
-      email: user.email,
-    })
-  } else {
-    res.status(400).json({message: "Invalid Login Details"})
-  }
-  
-})
-
-//Create Restaurants
-
-//
 
